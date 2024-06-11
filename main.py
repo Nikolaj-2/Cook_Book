2202f25625de2923e7fc0a1565d0b388cbf22656
Nikolaj-2 <148116061+Nikolaj-2@users.noreply.github.com>
2024-06-11 13:42:08 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-import tkinter as tk
-from recipe_book.recipe_book_app import RecipeBookApp
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = RecipeBookApp(root)
-    root.mainloop()
